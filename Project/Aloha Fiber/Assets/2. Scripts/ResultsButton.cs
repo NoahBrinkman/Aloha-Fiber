2d4e650c1886188faaa407335da0763277d8fd86
Noah <Noahpbrinkman@gmail.com>
2021-05-21 10:59:59 +0200

    Epic C setup

---
+++ Project/Aloha Fiber/Assets/2. Scripts/ResultsButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ResultsButton : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClicked);
+    }
+
+    private void OnClicked()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
